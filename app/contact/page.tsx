8c2e3460d1d17d6beb47cf58c5fffd20ceda4ca1
dibya-22 <dibyalochanpattnayak045@gmail.com>
2026-05-31 00:33:40 +0530

    feat: add contact page: placeholder

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+
+const Contact = () => {
+    return (
+        <div className="h-screen flex justify-center items-center bg-background text-foreground">
+            Don&#39;t worry! I will work on it soon.
+        </div>
+    )
+}
+
+export default Contact
