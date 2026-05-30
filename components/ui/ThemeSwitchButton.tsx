3a850be113b304290c8d157330c68bdc13e68dfe
dibya-22 <dibyalochanpattnayak045@gmail.com>
2026-05-30 16:51:10 +0530

    chore: add type button for better accessibility

--- components/ui/ThemeSwitchButton.tsx
+++ components/ui/ThemeSwitchButton.tsx
@@ -9,6 +9,7 @@ export function ThemeSwitcher() {
 
     return (
         <motion.button
+            type="button"
             whileHover={{ scale: 1.1 }}
             transition={{ type: "spring", stiffness: 400, damping: 20 }}
             onClick={() => setTheme(isDark ? "light" : "dark")}
